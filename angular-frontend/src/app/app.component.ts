7904c2e3d1a6015044ae093c532103f511be8820
Sascha Meier <meiers.tin22@dhbw-heidenheim.de>
2024-02-16 01:40:29 +0100

    Added Mealplan Component

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',  
 })
 export class AppComponent {
   title = 'homes';
---
+++ angular-frontend/src/app/mealplan/mealplan.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mealplan',
+  templateUrl: './mealplan.component.html',
+  styleUrl: './mealplan.component.css'
+})
+export class MealplanComponent {
+
+}
