3a57d21b18553581e37749d2dba83f8f5b4a39a2
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-02-16 13:08:52 +0100

    update doc and db model

--- springboot-backend/src/main/java/com/app/kantinerado/models/mealplan/DishCategory.java
+++ springboot-backend/src/main/java/com/app/kantinerado/models/mealplan/DishCategory.java
@@ -1,5 +1,4 @@
 package com.app.kantinerado.models.mealplan;
 
-public enum DishCategory {
-    MENÜ_1, MENÜ_2, DESSERT, SOUP,
+public class DishCategory {
 }
---
+++ springboot-backend/src/main/java/com/app/kantinerado/repository/DishCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.app.kantinerado.repository;
+
+import com.app.kantinerado.models.mealplan.Day;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DishCategoryRepository extends JpaRepository<Day, Integer> {
+
+}
