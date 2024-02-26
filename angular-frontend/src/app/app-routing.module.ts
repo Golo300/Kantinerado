d496af030cc041560d16d4b15b588519331ea87d
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-02-26 21:46:20 +0100

    added routing

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MealplanComponent } from './mealplan/mealplan.component';
+import { MealplanOrderComponent } from './mealplan-order/mealplan-order.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 const routes: Routes = [
-  { path: 'mealplan', component: MealplanComponent },
+  { path: '', component: MealplanComponent },
+  { path: 'order', component: MealplanOrderComponent },
+  { path: 'checkout', component: CheckoutComponent },
   // Weitere Routen können hier hinzugefügt werden
 ];
 
