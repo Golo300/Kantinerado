361824763fb23fa1a960f9bca1d917919a8fb055
Sascha Meier <meiers.tin22@dhbw-heidenheim.de>
2024-02-26 15:00:56 +0100

    added checkout, updated kw font

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MealplanComponent } from './mealplan/mealplan.component';
 import { MealplanOrderComponent } from './mealplan-order/mealplan-order.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MealplanComponent,
-    MealplanOrderComponent
+    MealplanOrderComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
