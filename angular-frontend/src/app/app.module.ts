003b90cae26351cb89993624a8a1c3170a17a041
Sascha Meier <meiers.tin22@dhbw-heidenheim.de>
2024-02-26 11:58:35 +0100

    added date-fns, outsourced mealplan-order and added new table features

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MealplanComponent } from './mealplan/mealplan.component';
+import { MealplanOrderComponent } from './mealplan-order/mealplan-order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MealplanComponent
+    MealplanComponent,
+    MealplanOrderComponent
   ],
   imports: [
     BrowserModule,
