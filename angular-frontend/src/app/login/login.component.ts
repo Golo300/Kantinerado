79be01083d6ef0302326b3795e48fb3db910316b
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-02-29 11:40:40 +0100

    test for interceptor

--- angular-frontend/src/app/login/login.component.ts
+++ angular-frontend/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { catchError, of, pipe, tap } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -17,15 +18,21 @@ export class LoginComponent {
   login(): void {
     console.log(this.username)
     this.authService.login({ username: this.username, password: this.password })
-      .subscribe(response => {
-        // Hier können Sie die Antwort des Authentifizierungsservices verarbeiten
-        console.log('Login erfolgreich:', response);
-        // Weiterleitung nach erfolgreichem Login
-        this.router.navigate(['/']); // Hier 'dashboard' durch Ihre Zielseite ersetzen
-      }, error => {
-        // Hier können Sie mit einem Fehler bei der Authentifizierung umgehen
-        console.error('Fehler beim Login:', error);
+      .pipe(
+        tap(() => console.log('Register erfolgreich')),
+        catchError(error => {
+          console.error('Fehler beim Login:', error);
         this.loginError = 'Fehler beim Login. Überprüfen Sie Ihre Anmeldeinformationen und versuchen Sie es erneut.';
+          return of(null);
+        })
+      )
+      .subscribe(response => {
+        if (response) {
+          console.log('Login erfolgreich:', response);
+        // Weiterleitung nach erfolgreichem Login
+        window.location.reload();
+        this.router.navigate(['/order']); // Hier 'dashboard' durch Ihre Zielseite ersetzen
+        }
       });
   }
 }
