52b448d36c4102799c038c8d8279eb1917db1c15
JonasG <jonas.graczikowski@zeiss.com>
2024-03-10 16:35:04 +0100

    Updated validation for order

--- springboot-backend/src/main/java/com/app/kantinerado/services/OrderService.java
+++ springboot-backend/src/main/java/com/app/kantinerado/services/OrderService.java
@@ -43,6 +43,19 @@ public class OrderService {
             return false;
         }
 
+        //Bestellung ist bis Donnerstag, 18:00 Uhr für die kommende Woche möglich
+        if (order.getDate().after(nextThursday18)) {
+            setMessage("Bestellung ist nur bis Donnerstag, 18:00 Uhr für die kommende Woche möglich");
+            return false;
+        }
+
+        //Samstags darf kein Menü 1 und keine Suppe bestellt werden
+        if (orderedDish.getDishCategory().getName().equals("Menü1") ||
+                orderedDish.getDishCategory().getName().equals("Suppe")) {
+            setMessage("Samstags darf kein Menü 1 und keine Suppe bestellt werden");
+            return false;
+        }
+
         Order newOrder = new Order(order, orderedDish, aplicationUser);
 
         try {
@@ -55,6 +68,7 @@ public class OrderService {
         return true;
     }
 
+    //gibt Datum und Uhrzeit des nächsten Donnerstags 18 Uhr zurück
     private static Date getNextThursday18() {
         Date currentDate = new Date();
         Calendar currentCalendarDate = Calendar.getInstance();
