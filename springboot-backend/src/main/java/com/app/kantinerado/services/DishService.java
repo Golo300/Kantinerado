1827d1a24febc13b34e61d5a2efb5f182d74c7ee
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-03-23 16:22:15 +0100

    get dishes by category

---
+++ springboot-backend/src/main/java/com/app/kantinerado/services/DishService.java
@@ -0,0 +1,31 @@
+package com.app.kantinerado.services;
+
+import com.app.kantinerado.models.mealplan.Dish;
+import com.app.kantinerado.models.mealplan.DishCategory;
+import com.app.kantinerado.repository.DishCategoryRepository;
+import com.app.kantinerado.repository.DishRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Transient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transient
+public class DishService {
+
+    @Autowired
+    private DishCategoryRepository dishCategoryRepository;
+
+    @Autowired
+    private DishRepository dishRepository;
+
+    public List<Dish> getDishesByCategory(String categoryName)
+    {
+        DishCategory category = dishCategoryRepository.findByName(categoryName).orElse(null);
+
+        if(category == null) return null;
+
+        return dishRepository.findByDishCategory(category);
+    }
+}
